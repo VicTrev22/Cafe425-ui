db60f5a6c9914a6760ac9f3093ab5246ae0d5cfa
Vicente Trevino <vicentetrevino752@gmail.com>
2024-03-29 20:35:46 -0400

    added mobile nav and footer

--- src/navigation-bar/navigation-bar.jsx
+++ src/navigation-bar/navigation-bar.jsx
@@ -1,8 +1,35 @@
+import { useState, useEffect } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 import "./navigation-bar.css";
+import MobileNavigation from "../mobile-navigation/mobile-navigation";
 
 function NavigationBar() {
+  const [show, setShow] = useState(false);
+
+  useEffect(() => {
+    function handleResize() {
+      // Automatically hide the mobile navigation and reset to the bar icon on larger screens
+      if (window.innerWidth > 768) {
+        setShow(false);
+      }
+    }
+
+    // Add resize event listener
+    window.addEventListener("resize", handleResize);
+
+    // Call the resize handler immediately to set the initial state correctly
+    handleResize();
+
+    // Cleanup by removing the event listener
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  const toggleMenu = () => {
+    console.log(show);
+    setShow(!show);
+  };
+
   return (
     <>
       <header className="header container">
@@ -30,9 +57,17 @@ function NavigationBar() {
               </a>
             </li>
           </ul>
-          <FontAwesomeIcon className="header-bar" icon={faBars} />
+          <div
+            className={`icon-container ${show ? "show" : ""}`}
+            onClick={toggleMenu}
+          >
+            <FontAwesomeIcon className="fa-bars" icon={faBars} />
+            <FontAwesomeIcon className="fa-times" icon={faTimes} />
+          </div>
         </nav>
       </header>
+      {/* Mobile Navigation */}
+      <MobileNavigation show={show}></MobileNavigation>
     </>
   );
 }
